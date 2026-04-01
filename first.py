40322137056915410a6613577acbbc88d99b776e
Gumis <123287374+machilan1@users.noreply.github.com>
2026-04-01 19:21:50 +0800

    Refactor main function to implement swimming assignment optimization with cost minimization

--- first.py
+++ first.py
@@ -5,11 +5,19 @@ from ortools.linear_solver import pywraplp
 
 def main():
     # Data
-    projects = [1, 2, 3, 4, 5, 6]
-    num_projects = len(projects)
 
-    cost_coeff = [38, 33, 39, 45, 23, 27]
-    profit_coeff = [15, 12, 16, 18, 9, 11]
+    persons = ["Carl", "Chris", "David", "Tony", "Ken"]
+    strokes = ["backstroke", "breaststroke", "butterfly", "freestyle"]
+    num_persons = len(persons)
+    num_strokes = len(strokes)
+
+    cost_coeff = [
+        [37.7, 43.4, 33.3, 29.2],
+        [32.9, 33.1, 28.5, 26.4],
+        [33.8, 42.2, 38.9, 29.6],
+        [37.0, 34.7, 30.4, 28.5],
+        [35.4, 41.8, 33.6, 31.1],
+    ]
 
     quota = 100
 
@@ -24,28 +32,26 @@ def main():
     # x[i] is an array of 0-1 variables, which will be 1
     # if worker i is assigned to task j.
     x = {}
-    for i in range(num_projects):
-        x[i] = solver.IntVar(0, 1, "")
+    for i in range(num_persons):
+        for j in range(num_strokes):
+            x[i, j] = solver.IntVar(0, 1, "")
 
     # Constraints
-    # 不要花超過quota
+    # 每個式只能有一個人上場
+    for j in range(num_strokes):
+        solver.Add(solver.Sum([x[i, j] for i in range(num_persons)]) == 1)
 
-    solver.Add(solver.Sum([x[i] * cost_coeff[i] for i in range(num_projects)]) <= quota)
-
-    # Investment opportunities 1 and 2 are mutually exclusive, and so are 3 and 4.
-    solver.Add(x[0] + x[1] <= 1)
-    solver.Add(x[2] + x[3] <= 1)
-
-    # neither 3 nor 4 can be undertaken unless one of the first two opportunities is undertaken.
-    solver.Add(x[0] + x[1] >= x[2])
-    solver.Add(x[0] + x[1] >= x[3])
+    # 一個人只頂多只能游一種式
+    for i in range(num_persons):
+        solver.Add(solver.Sum(x[i, j] for j in range(num_strokes)) <= 1)
 
     # Objective
 
     objective_terms = []
-    for i in range(num_projects):
-        objective_terms.append(x[i] * profit_coeff[i])
-    solver.Maximize(solver.Sum(objective_terms))
+    for i in range(num_persons):
+        for j in range(num_strokes):
+            objective_terms.append(x[i, j] * cost_coeff[i][j])
+    solver.Minimize(solver.Sum(objective_terms))
 
     # Solve
     print(f"Solving with {solver.SolverVersion()}")
@@ -55,10 +61,14 @@ def main():
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
         total_cost = 0
         print(f"Total value = {solver.Objective().Value()}\n")
-        for i in range(num_projects):
-            if x[i].solution_value() > 0.5:
-                total_cost += cost_coeff[i]
-                print(f"Project {projects[i]} is picked." + f" Cost: {cost_coeff[i]}")
+        for i in range(num_persons):
+            for j in range(num_strokes):
+                if x[i, j].solution_value() > 0.5:
+                    total_cost += cost_coeff[i][j]
+                    print(
+                        f"Swimmer {persons[i]} is picked for {strokes[j]}"
+                        + f" Cost: {cost_coeff[i][j]}"
+                    )
         print(f"Total cost = {total_cost}\n")
     else:
         print("No solution found.")
