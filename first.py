93156f410a93872faadea1abd4f70895e5bca6b8
Gumis <123287374+machilan1@users.noreply.github.com>
2026-04-01 17:19:22 +0800

    Refactor chore assignment logic and constraints for improved clarity and functionality

--- first.py
+++ first.py
@@ -5,24 +5,13 @@ from ortools.linear_solver import pywraplp
 
 def main():
     # Data
-    routes: List[str] = [
-        "San Francisco to Los Angeles",
-        "San Francisco to Denver",
-        "San Francisco to Seattle",
-        "Los Angeles to Chicago",
-        "Los Angeles to San Francisco",
-        "Chicago to Denver",
-        "Chicago to Seattle",
-        "Denver to San Francisco",
-        "Denver to Chicago",
-        "Seattle to San Francisco",
-        "Seattle to Los Angeles",
-    ]
+    chores = ["marketing", "cooking", "dishWashing", "laundry"]
+    person = ["Eve", "Steven"]
 
-    sequences = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    num_chores = len(chores)
+    num_person = len(person)
 
-    num_sequences = len(sequences)
-    costs = [2, 3, 4, 6, 7, 5, 7, 8, 9, 9, 8, 9]
+    cost_params = [[4.5, 7.8, 3.6, 2.9], [4.9, 7.2, 4.3, 3.1]]
 
     # Solver
     # Create the mip solver with the SCIP backend.
@@ -34,48 +23,29 @@ def main():
     # Variables
     # x[i] is an array of 0-1 variables, which will be 1
     # if worker i is assigned to task j.
+
     x = {}
-    for i in range(num_sequences):
-        x[i] = solver.IntVar(0, 1, "")
+    for i in range(num_person):
+        for j in range(num_chores):
+            x[i, j] = solver.IntVar(0, 1, "")
 
     # Constraints
+    # 兩個人都有兩件工作
+    for i in range(num_person):
+        solver.Add(solver.Sum([x[i, j] for j in range(num_chores)]) == 2)
 
-    # Set partitioning mode
-    # 每條路線要剛好走一次
-    solver.Add(solver.Sum([x[i] for i in [0, 3, 6, 9]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [1, 4, 7, 10]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [2, 5, 8, 11]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [3, 6, 8, 9, 11]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [0, 5, 9, 10]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [3, 4, 8]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [6, 7, 9, 10, 11]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [1, 3, 4, 8]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [4, 7, 10]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [2, 6, 7, 11]]) == 1)
-    solver.Add(solver.Sum([x[i] for i in [5, 8, 9, 10, 11]]) == 1)
+    # 每件工作都要有人完成
 
-    # Set covering mode
-    # 每條路線至少要走過一次
-    # solver.Add(solver.Sum([x[i] for i in [0, 3, 6, 9]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [1, 4, 7, 10]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [2, 5, 8, 11]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [3, 6, 8, 9, 11]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [0, 5, 9, 10]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [3, 4, 8]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [6, 7, 9, 10, 11]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [1, 3, 4, 8]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [4, 7, 10]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [2, 6, 7, 11]]) >= 1)
-    # solver.Add(solver.Sum([x[i] for i in [5, 8, 9, 10, 11]]) >= 1)
-
-    # Assign給3個crew
-    solver.Add(solver.Sum([x[i] for i in range(num_sequences)]) == 3)
+    for j in range(num_chores):
+        solver.Add(solver.Sum([x[i, j] for i in range(num_person)]) == 1)
 
     # Objective
 
     objective_terms = []
-    for i in range(num_sequences):
-        objective_terms.append(x[i] * costs[i])
+    for i in range(num_person):
+        for j in range(num_chores):
+            objective_terms.append(x[i, j] * cost_params[i][j])
+
     solver.Minimize(solver.Sum(objective_terms))
 
     # Solve
@@ -85,9 +55,13 @@ def main():
     # Print solution.
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
         print(f"Total value = {solver.Objective().Value()}\n")
-        for i in range(num_sequences):
-            if x[i].solution_value() > 0.5:
-                print(f"Sequence {sequences[i]} is picked." + f" Cost: {costs[i]}")
+        for i in range(num_person):
+            for j in range(num_chores):
+                if x[i, j].solution_value() > 0.5:
+                    print(
+                        f"Person {person[i]} chooses to do {chores[j]}."
+                        + f" Cost: {cost_params[i][j]}"
+                    )
     else:
         print("No solution found.")
 
